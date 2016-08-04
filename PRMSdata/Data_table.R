57fc88caa131e2ba91ee2ce4872585e91128f803
abock <abock@usgs.gov>
2016-08-04 16:20:09 -0600

    Demo of data table capabilities in shiny

---
+++ PRMSdata/Data_table.R
@@ -0,0 +1,56 @@
+
+# will need to have data table as a second tab
+# Create example data
+library(shiny)
+
+ui<-shinyUI(navbarPage(
+  title = 'DataTable Options',
+  tabPanel('Display length',     DT::dataTableOutput('ex1')),
+  tabPanel('Length menu',        DT::dataTableOutput('ex2')),
+  tabPanel('No pagination',      DT::dataTableOutput('ex3')),
+  tabPanel('No filtering',       DT::dataTableOutput('ex4')),
+  tabPanel('Function callback',  DT::dataTableOutput('ex5'))
+))
+
+
+server<-shinyServer(function(input, output) {
+  
+  # display 10 rows initially
+  output$ex1 <- DT::renderDataTable(
+    DT::datatable(FutMM, options = list(pageLength = 12),colnames=c("Month","GCM1","GCM2","Mean"))
+  )
+  
+# -1 means no pagination; the 2nd element contains menu labels
+output$ex2 <- DT::renderDataTable(
+  DT::datatable(
+    FutMM, options = list(
+      lengthMenu = list(c(4, 12, -1), c('4', '12', 'All')),
+      pageLength = 12
+    )
+  )
+)
+
+# you can also use paging = FALSE to disable pagination
+output$ex3 <- DT::renderDataTable(
+  DT::datatable(FutMM, options = list(paging = FALSE))
+)
+
+  # turn off filtering (no searching boxes)
+  output$ex4 <- DT::renderDataTable(
+    DT::datatable(FutMM, options = list(searching = FALSE))
+  )
+
+  # write literal JS code in JS()
+  output$ex5 <- DT::renderDataTable(DT::datatable(
+    FutMM,
+    options = list(rowCallback = DT::JS(
+      'function(row, data) {
+      // Bold cells for those >= 5 in the first column
+      if (parseFloat(data[1]) >= 5.0)
+      $("td:eq(1)", row).css("font-weight", "bold");
+}'
+    ))
+    ))
+  })
+
+shinyApp(ui = ui, server = server)
