115ba8b7dc11e724b391d2c85ccde47de70ccf89
abock <abock@usgs.gov>
2016-08-16 13:10:10 -0600

    experimented with side panel layout to add streamflow variable

--- PRMSdata/SidePanel_Year_GCM_selection.R
+++ PRMSdata/SidePanel_Year_GCM_selection.R
@@ -5,6 +5,9 @@ y2055=c('ECHAM5','GENMON','GFDL','Mean')
 y2080=c('ECHAM5','GENMON','Mean',NA)
 dd2<-data.frame(y2030,y2055,y2080)
 yrs<-c('2030','2055','2080')
+SF_vars<-c("Qann", "QSpring","QSummer","QFall","QWinter","QJanuary",
+          "QFebruary","QMarch",'QApril',"QMay","QJune","Qjuly",
+          "QAugust","QSeptember","QOctober","QNovember","QDecember")
 
 ui<-shinyUI(fluidPage(
 
@@ -19,18 +22,29 @@ ui<-shinyUI(fluidPage(
       textOutput("text1"),
       textOutput("text2"),
       textOutput("text3"),
-      textOutput("text4")
+      textOutput("text4"),
+      textOutput("text5")
       
     ),
     sidebarPanel(
       
-      selectInput("select", label = h3("Select box"), 
+      selectInput("select", label = h3("Year"), 
                   choices = list("2030" = 1, "2055" = 2,
                                  "2080" = 3), selected = 1),
   
       actionButton("do", "Select Year"),
       
-      radioButtons("GCMnames", label="GCM Names", choices="",selected="")
+      radioButtons("GCMnames", label="GCM Names", choices="",selected=""),
+      
+      selectInput("select2", label = h3("Streamflow Variable"), 
+                  choices = list("Qann" = 1, "QSpring" = 2,"QSummer" = 3,
+                                 "QFall"=4,"QWinter"=5,"QJanuary"=6,
+                                 "QFebruary"=7,"QMarch"=8,'QApril'=9,
+                                 "QMay"=10,"QJune"=11,"Qjuly"=12,
+                                 "QAugust"=13,"QSeptember"=14,"QOctober"=15,
+                                 "QNovember"=16,"QDecember"=17), selected = 1),
+     
+      actionButton("do2", "Select Variable")
      
     )
  ))
@@ -57,6 +71,7 @@ server<-shinyServer(function(input, output, session){
     return(yr)
   })
   
+  
   output$text1 <- renderText({ 
     input$GCMnames
     #input$select
@@ -72,6 +87,10 @@ server<-shinyServer(function(input, output, session){
   
   output$text4 <- renderText({ 
     summary(datum())
+  })
+  
+  output$text5 <- renderText({ 
+    input$do2
   })
   
 })
