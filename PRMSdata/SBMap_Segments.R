22191987c0698dd332bf8c3ae1d51243254aa437
abock <abock@usgs.gov>
2016-08-19 15:55:06 -0600

    Finished data aggregation

--- PRMSdata/SBMap_Segments.R
+++ PRMSdata/SBMap_Segments.R
@@ -18,72 +18,6 @@ children<-sbtools::item_list_children(PRMS_item)
 sFlow_SBid<-tail(unlist(strsplit(children[[2]]$link$url,"/")),1)
 #Create object for the SFlow data
 child_item<-sbtools::item_get(sFlow_SBid)
-
-#********************************************************************************************************
-#********************************************************************************************************
-# List the streamflow files from Appendix2
-sbFiles<-sbtools::item_list_files(child_item)
-print(sbFiles$fname)
-
-
-# find files based on grep of basin name, year, and gcm name
-baseLine<-grep(paste(c("1982","BASELINE"),collapse="_"),sbFiles$fname)
-# one GCM for all sites
-#future<-grep(paste(c("2055","GFDL"),collapse="_"),sbFiles$fname)
-# all GCMs for all sites
-future_All_2030<-grep(paste(c("2030"),collapse="_"),sbFiles$fname)
-future_All_2055<-grep(paste(c("2055"),collapse="_"),sbFiles$fname)
-future_All_2080<-grep(paste(c("2080"),collapse="_"),sbFiles$fname)
-
-# Hardcoded wastershed and GCM names for column names
-msites<-c("OF","RW","LD","MM","JD","CD","BT")
-# only 2 gcms for 2030, 3 for 2055, 2 for 2080
-gcms<-c("ECHAM5","GENMON","GFDL")
-
-baseData<-sb2DF(baseLine,msites)
-avgBase<-colMeans(baseData)
-
-cNames2030<-unlist(lapply(msites,function(x) paste(x,"_",gcms[1:2],sep="")))
-futData2030<-sb2DF(future_All_2030,cNames2030)
-Avg2030<-avGCM(futData2030,gcms[1:2],"2030")
-dep2030<-sweep(Avg2030,1,avgBase)*100
-#MM2030
-#Sea2030
-
-cNames2055<-unlist(lapply(msites,function(x) paste(x,"_",gcms,sep="")))
-futData2055<-sb2DF(future_All_2055,cNames2055)
-Avg2055<-avGCM(futData2055,gcms,"2055")
-dep2055<-sweep(Avg2055,1,avgBase)*100
-#MM2055
-#Sea2055
-
-cNames2080<-unlist(lapply(msites,function(x) paste(x,"_",gcms[1:2],sep="")))
-futData2080<-sb2DF(future_All_2080,cNames2080)
-Avg2080<-avGCM(futData2080,gcms[1:2],"2080")
-dep2080<-sweep(Avg2080,1,avgBase)*100
-#MM2070
-#Sea2070
-
-AvgAll<-cbind(Avg2030,Avg2055,Avg2080)
-depAll<-cbind(dep2030,dep2055,dep2080)
-
-#Get the means and change from baseline for each feature
-perChange<-((AvgAll$MEAN_2030-avgBase)/avgBase)*100
-#********************************************************************************************************
-#********************************************************************************************************
-# Might need to make this a reactive function
-
-# Operations to retrieve data for a single segment and multiple GCMs using O'Fallon Creek as an example
-#baseLine_OF<-grep(paste(c("OF","1982","BASELINE"),collapse="_"),sbFiles$fname)
-baseLine_BT<-baseData[,grep("seg.17_BT",names(baseData))]
-#future_OF<-grep(paste(c("OF","2055"),collapse="_"),sbFiles$fname)
-future_BT<-futData2030[,grep("seg.17_BT",names(futData2030))]
-future_BT$means<-rowMeans(future_BT)
-dep_BT<-sweep(future_BT,1,baseLine_BT)*100
-
-# get mean monthly data
-FutMM <- aggregate(dep_BT, list(data.table::month(as.Date(rownames(dep_BT)))), mean,na.rm=T)
-
 #*******************************************************************************************
 #*******************************************************************************************
 # Mapping Components
@@ -111,37 +45,77 @@ finalSegs@data$ID<-c(1:185)
 
 # gets the xy points of each line
 res <- lapply(slot(finalSegs, "lines"), function(x) lapply(slot(x, "Lines"),
-                                                     function(y) slot(y, "coords")))
+                                                           function(y) slot(y, "coords")))
 #
 test<-lapply(res, function(x) c(max(unlist(x)),min(unlist(x))))
 Lats<-unlist(test)[c(TRUE,FALSE)]
 Longs<-unlist(test)[c(FALSE,TRUE)]
+#********************************************************************************************************
+#********************************************************************************************************
+# List the streamflow files from Appendix2
+sbFiles<-sbtools::item_list_files(child_item)
+print(sbFiles$fname)
+
+# find files based on grep of basin name, year, and gcm name
+baseLine<-grep(paste(c("1982","BASELINE"),collapse="_"),sbFiles$fname)
+# one GCM for all sites
+#future<-grep(paste(c("2055","GFDL"),collapse="_"),sbFiles$fname)
+# all GCMs for all sites
+future_All_2030<-grep(paste(c("2030"),collapse="_"),sbFiles$fname)
+future_All_2055<-grep(paste(c("2055"),collapse="_"),sbFiles$fname)
+future_All_2080<-grep(paste(c("2080"),collapse="_"),sbFiles$fname)
+
+# Hardcoded wastershed and GCM names for column names
+msites<-c("OF","RW","LD","MM","JD","CD","BT")
+# only 2 gcms for 2030, 3 for 2055, 2 for 2080
+gcms<-c("ECHAM5","GENMON","GFDL")
+
+baseData<-sb2DF(baseLine,msites)
+avgBase<-colMeans(baseData)
+
+cNames2030<-unlist(lapply(msites,function(x) paste(x,"_",gcms[1:2],sep="")))
+futData2030<-sb2DF(future_All_2030,cNames2030)
+Avg2030<-avGCM(futData2030,gcms[1:2],"2030")
+dep2030<-sweep(Avg2030,1,avgBase)*100
+
+cNames2055<-unlist(lapply(msites,function(x) paste(x,"_",gcms,sep="")))
+futData2055<-sb2DF(future_All_2055,cNames2055)
+Avg2055<-avGCM(futData2055,gcms,"2055")
+dep2055<-sweep(Avg2055,1,avgBase)*100
+
+cNames2080<-unlist(lapply(msites,function(x) paste(x,"_",gcms[1:2],sep="")))
+futData2080<-sb2DF(future_All_2080,cNames2080)
+Avg2080<-avGCM(futData2080,gcms[1:2],"2080")
+dep2080<-sweep(Avg2080,1,avgBase)*100
+
+#AvgAll<-cbind(Avg2030,Avg2055,Avg2080)
+depAll<<-cbind(dep2030,dep2055,dep2080)
+rownames(depAll)<-colnames(baseData)
+
+#********************************************************************************************************
+#********************************************************************************************************
+# Seasonal aggregation
+baseSeas<-t(seasonalMeans_Base(baseData))
+Seas2030<-seasonalMeans(futData2030,baseSeas)
+depS2030<-avGCM_Seas(Seas2030,gcms[1:2],2030)
+Seas2055<-seasonalMeans(futData2055,baseSeas)
+depS2055<-avGCM_Seas(Seas2055,gcms,2055)
+Seas2080<-seasonalMeans(futData2080,baseSeas)
+depS2080<-avGCM_Seas(Seas2080,gcms[1:2],2080)
+depSeason<<-cbind(depS2030,depS2055,depS2080)
 
 
-#************JUNK*********************************
-#************JUNK*********************************
-# # retrieve baseline data for all sites
-# baseFlow<-lapply(sbFiles$url[baseLine],RCurl::getURL)
-# # function to open all files as data frames
-# baseAll<-lapply(baseFlow,function(i){
-#   read.csv(text=unlist(i),row.names=1)
-# })
-# # Add column names 
-# baseAll<-mapply(colN,baseAll,msites,SIMPLIFY=F)
-# # bind the data frames together
-# baseData<-dplyr::bind_cols(baseAll)
-# # Add timestamps as row names
-# row.names(baseData)<-rownames(baseAll[[1]])
+## Mean monthly aggregation
+baseData$date<-rownames(baseData)
+baseData_zoo<-zoo::read.zoo(baseData,index.column=186)
+baseData_MM<-t(aggregate(baseData_zoo,function(x) cycle(zoo::as.yearmon(x)),na.rm=T))
+MM2030<-t(MeanMonthly(futData2030,baseData_MM))
+depMM2030<-avGCM_MM(MM2030,gcms[1:2],2030)
+MM2055<-t(MeanMonthly(futData2055,baseData_MM))
+depMM2055<-avGCM_MM(MM2055,gcms,2055)
+MM2080<-t(MeanMonthly(futData2080,baseData_MM))
+depMM2080<-avGCM_MM(MM2080,gcms[1:2],2080)
 
-# # Perform the same tasks, but for the GCM data
-# futFlow<-lapply(sbFiles$url[future],RCurl::getURL)
-# futAll<-lapply(futFlow,function(i){
-#   read.csv(text=unlist(i),row.names=1)
-# })
-# futAll<-mapply(colN,futAll,msites,SIMPLIFY=F)
-# futData<-dplyr::bind_cols(futAll)
-# rownames(futData)<-rownames(futAll[[1]])
+depMM<<-cbind(depMM2030,depMM2055,depMM2080)
 
-## remove segments 32 and 34 from the data frames (MM river)
-#baseData<-baseData[,!colnames(baseData) %in% c("seg.32_MM","seg.34_MM")]
-#futData<-futData[,!colnames(futData) %in% c("seg.32_MM","seg.34_MM")]
+dFrame<<-cbind(depAll,depSeason,depMM)
